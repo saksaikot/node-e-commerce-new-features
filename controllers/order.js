93cc3da80ecbee4beeca928b0fb6ccc9bf34f8e5
Sultan Ahmad Khan <saksaikot@gmail.com>
2022-01-05 13:46:36 +0600

    renamed server to index

--- controllers/order.js
+++ controllers/order.js
@@ -2,9 +2,11 @@ const { Order } = require("../models/order");
 const { _pick } = require("../helper/lodash");
 
 const index = async function (req, res) {
-  const order = await Order.find({ userId: req.user._id }).select({
-    sessionKey: 0,
-  });
+  const order = await Order.find({ userId: req.user._id })
+    .populate({ path: "cartItems.product", select: "name" })
+    .select({
+      sessionKey: 0,
+    });
   return res.status(200).send(order);
 };
 
